054d0ac7863d5e360677e5f42a5be663379be68a
Lucas <lcatolino@fi.uba.ar>
2022-05-05 19:43:42 -0300

    Add radiation system

---
+++ src/systems/Radiation.java
@@ -0,0 +1,29 @@
+package systems;
+
+import java.util.List;
+
+import algorithms.Algorithm;
+import models.Particle;
+import models.Vector;
+
+public class Radiation implements SystemType {
+
+	@Override
+	public Vector getForce(Particle particle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Vector> getRDerivatives(int order, Particle particle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Particle> simulate(Algorithm algorithm, double ts, double tmax) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
